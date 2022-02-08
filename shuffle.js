71edc18b596afc251ce2cece759cc69e9b72317c
Claudia Flores <cflores1@email.arizona.edu>
2022-02-07 22:34:08 -0700

    leetcode shuffle array problem

---
+++ shuffle.js
@@ -0,0 +1,39 @@
+/*
+Given the array nums consisting of 2n elements in the form [x1,x2,...,xn,y1,y2,...,yn].
+
+Return the array in the form [x1,y1,x2,y2,...,xn,yn].
+
+Example 1:
+Input: nums = [2,5,1,3,4,7], n = 3
+Output: [2,3,5,4,1,7] 
+Explanation: Since x1=2, x2=5, x3=1, y1=3, y2=4, y3=7 then the answer is [2,3,5,4,1,7].
+
+Example 2:
+Input: nums = [1,2,3,4,4,3,2,1], n = 4
+Output: [1,4,2,3,3,2,4,1]
+
+Example 3:
+Input: nums = [1,1,2,2], n = 2
+Output: [1,2,1,2]
+ 
+
+Constraints:
+1 <= n <= 500
+nums.length == 2n
+1 <= nums[i] <= 10^3
+*/
+
+
+const shuffle = (nums, n) => {
+    //empty array that we will call once we push nums into the array
+    let result = [];
+    
+    //loop through the array until it reaches n
+    for (i = 0; i < n; i++) {
+        //push the nums in the array to the new array
+        //we can add i+n because n is the halfway point
+        result.push(nums[i],nums[i+n]);
+    }
+    return result
+}
+console.log(shuffle)
